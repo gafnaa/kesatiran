994fccaf4a37d97c481a99bd5539b0586aa6210e
gafnaa <gafnaalfaatiha@gmail.com>
2025-05-16 00:49:28 +0700

    nambah framer transition

--- src/components/Stairs.jsx
+++ src/components/Stairs.jsx
@@ -1,4 +1,16 @@
+import { motion } from "framer-motion"
 
+const stairAnimation = {
+    initial: {
+        top: "0%",
+    },
+    animate: {
+        top: "100%",
+    },
+    exit: {
+        top: ["100%", "0%"]
+    }
+}
 
 const Stairs = () => {
   return (
