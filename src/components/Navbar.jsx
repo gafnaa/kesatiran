0aca888ef1ad45ed21e20628f6ffa835a2802dbe
gafnaa <gafnaalfaatiha@gmail.com>
2025-05-21 12:45:23 +0700

    fix: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,43 @@
-// components/Navbar.jsx
-// Hanya untuk navigasi desktop
-'use client'; // Masih perlu karena menggunakan motion.a
+'use client';
 
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { motion } from 'framer-motion';
-import Link from 'next/link'; // Gunakan Link dari Next.js
+
+const links = [
+  { name: 'PROJECTS', path: '/projects' },
+  { name: 'ABOUT US', path: '/about' },
+  { name: 'CONTACT', path: '/contact' },
+];
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
-    // Navigasi Desktop
-    <nav className="hidden md:flex space-x-6 items-center"> {/* Tambahkan items-center untuk alignment */}
-      <motion.div // Bungkus Link dengan motion.div jika ingin animasi
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.1 }}
-      >
-        <Link href="/projects" className="hover:text-red-accent transition duration-300">
-          PROJECTS
-        </Link>
-      </motion.div>
+    <nav className="hidden md:flex space-x-6 items-center py-8">
+      {links.map((link, index) => {
+        const isActive = pathname === link.path;
 
-       <motion.div
-         initial={{ opacity: 0, y: -20 }}
-         animate={{ opacity: 1, y: 0 }}
-         transition={{ duration: 0.5, delay: 0.2 }}
-       >
-         <Link href="/about" className="hover:text-red-accent transition duration-300">
-           ABOUT US
-         </Link>
-       </motion.div>
-
-      <motion.div
-         initial={{ opacity: 0, y: -20 }}
-         animate={{ opacity: 1, y: 0 }}
-         transition={{ duration: 0.5, delay: 0.3 }}
-      >
-         <Link href="/contact" className="hover:text-red-accent transition duration-300">
-           CONTACT
-         </Link>
-       </motion.div>
+        return (
+          <motion.div
+            key={index}
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.1 * (index + 1) }}
+          >
+            <Link
+              href={link.path}
+              className={`relative capitalize font-light transition-colors duration-300 
+                ${isActive ? 'text-[#FF1D1D]' : 'text-white hover:text-[#FF1D1D]'}`}
+            >
+              {link.name}
+              {isActive && (
+                <span className="absolute bottom-[-4px] left-0 h-[2px] w-full bg-[#FF1D1D] transition-all duration-500 ease-in-out" />
+              )}
+            </Link>
+          </motion.div>
+        );
+      })}
     </nav>
   );
 };
